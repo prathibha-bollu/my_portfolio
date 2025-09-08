afd678dba36fdc5e6abb23e36b8318da327a745f
prathibha-bollu <bprathibhaedu@gmail.com>
2025-09-08 12:59:25 +0300

    router issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/my_portfolio/' : '/', // <-- only use /MealPlan/ for production
-  plugins: [react()],
+  base: mode === 'production' ? '/my_portfolio/' : '/',
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
